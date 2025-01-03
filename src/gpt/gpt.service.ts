21ac004e6166b08d5f1403e1eb436da1f07e662c
Carlos Rojas Burgos <cjrojasb@falabella.cl>
2025-01-03 15:28:41 -0300

    openai was implemented for handle orthography check

--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { orthographyCheckUseCase } from './use-cases/orthography.use-case';
 import { OrthographyDto } from './dto/orthography.dto';
+import OpenAI from 'openai';
 
 @Injectable()
 export class GptService {
+  private openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
   // only for handle use cases
   async orthographyCheck(orthographyDto: OrthographyDto) {
-    return await orthographyCheckUseCase({ prompt: orthographyDto.prompt });
+    return await orthographyCheckUseCase(this.openai, {
+      prompt: orthographyDto.prompt,
+    });
   }
 }
--- src/gpt/use-cases/orthography.use-case.ts
+++ src/gpt/use-cases/orthography.use-case.ts
@@ -1,10 +1,45 @@
+import OpenAI from 'openai';
+
 interface Options {
   prompt: string;
 }
 
-export const orthographyCheckUseCase = async ({ prompt }: Options) => {
-  return {
-    message: prompt,
-    apiKey: process.env.OPENAI_API_KEY,
-  };
+export const orthographyCheckUseCase = async (
+  openai: OpenAI,
+  { prompt }: Options,
+) => {
+  try {
+    const completion = await openai.chat.completions.create({
+      model: 'gpt-4o',
+      messages: [
+        {
+          role: 'system',
+          content: `Te serán proveídos textos en español, con posibles errores ortográficos y gramaticales, Debes de responder en formato JSON, tu tarea es corregirlos y retornar información con soluciones, también debes de dar un porcentaje de acierto por el usuario, Si no hay errores, debes de retornar un mensaje alegre de felicitaciones en ingles
+          
+          Ejemplo de salida:
+
+          {
+            userScore: number,
+            errors: string[], // ['error => solución'],
+            message: string // Usa emojis y texto para felicitar al usuario recuerda en ingles
+          }
+          `,
+        },
+        {
+          role: 'user',
+          content: prompt,
+        },
+      ],
+      temperature: 0.3,
+      max_tokens: 100,
+      response_format: {
+        type: 'json_object',
+      },
+    });
+
+    return JSON.parse(completion.choices[0].message.content);
+  } catch (error) {
+    console.error('Error:', error);
+    throw new Error('Connection error');
+  }
 };
