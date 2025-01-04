ed42243a8965bc033b477975cd1b4ad88fbfcb8a
Carlos Rojas Burgos <cjrojasb@falabella.cl>
2025-01-04 17:55:31 -0300

    pros-cons-discusser and pros-cons-discusser-stream methods was implemented into controller, service and useCase

--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { orthographyCheckUseCase } from './use-cases/orthography.use-case';
 import { OrthographyDto } from './dto/orthography.dto';
+import { ProsConsDiscusserDto } from './dto/pro-cons-discusser.dto';
+import { prosConsDiscusserStreamUseCase } from './use-cases/pros-cons-discusser-stream.use-case';
+import { prosConsDiscusserUseCase } from './use-cases/pros-cons-discusser.use-case';
 import OpenAI from 'openai';
 
 @Injectable()
@@ -17,6 +20,18 @@ export class GptService {
   async orthographyCheck(orthographyDto: OrthographyDto) {
     return await orthographyCheckUseCase(this.openai, {
       prompt: orthographyDto.prompt,
+    });
+  }
+
+  async prosConsDiscusser(prosConsDiscusserDto: ProsConsDiscusserDto) {
+    return await prosConsDiscusserUseCase(this.openai, {
+      prompt: prosConsDiscusserDto.prompt,
+    });
+  }
+
+  async prosConsDiscusserStream(prosConsDiscusserDto: ProsConsDiscusserDto) {
+    return await prosConsDiscusserStreamUseCase(this.openai, {
+      prompt: prosConsDiscusserDto.prompt,
     });
   }
 }
---
+++ src/gpt/use-cases/pros-cons-discusser-stream.use-case.ts
@@ -0,0 +1,32 @@
+import OpenAI from 'openai';
+
+interface Options {
+  prompt: string;
+}
+
+export const prosConsDiscusserStreamUseCase = async (
+  openai: OpenAI,
+  { prompt }: Options,
+) => {
+  try {
+    return await openai.chat.completions.create({
+      stream: true,
+      model: 'gpt-4o',
+      messages: [
+        {
+          role: 'system',
+          content: `Se te dará una pregunta y tu tarea es dar una respuesta con pros y contras, la respuesta debe de ser en formato markdown,  los pros y contras deben de estar en una lista.`,
+        },
+        {
+          role: 'user',
+          content: prompt,
+        },
+      ],
+      temperature: 0.8,
+      max_tokens: 500,
+    });
+  } catch (error) {
+    console.error('Error:', error);
+    throw new Error('Connection error');
+  }
+};
