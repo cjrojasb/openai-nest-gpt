ddc36a9cab2dc7ffa78fdb253d345daa7bf3cfa1
Carlos Rojas Burgos <cjrojasb@falabella.cl>
2025-01-08 18:11:21 -0300

    progress in edit image

--- src/gpt/use-cases/image-generation.use-case.ts
+++ src/gpt/use-cases/image-generation.use-case.ts
@@ -1,5 +1,10 @@
+import * as fs from 'fs';
+import * as path from 'path';
 import OpenAI from 'openai';
-import { downloadImageAsPng } from 'src/helpers/download-image-as-png';
+import {
+  downloadBase64ImageAsPng,
+  downloadImageAsPng,
+} from 'src/helpers/download-image-as-png';
 
 interface Options {
   prompt: string;
@@ -11,6 +16,18 @@ export const imageGenerationUseCase = async (
   openai: OpenAI,
   { prompt, originalImage, maskImage }: Options,
 ) => {
+  try {
+    if (!originalImage || !maskImage) {
+      return generateImage(openai, prompt);
+    }
+    return editImage(openai, { prompt, originalImage, maskImage });
+  } catch (error) {
+    console.error('Error:', error);
+    throw new Error('Connection error in image generation');
+  }
+};
+
+const generateImage = async (openai: OpenAI, prompt: string) => {
   const response = await openai.images.generate({
     model: 'dall-e-3',
     prompt: prompt,
@@ -20,11 +37,41 @@ export const imageGenerationUseCase = async (
     response_format: 'url',
   });
 
-  const url = response.data[0].url;
-  await downloadImageAsPng(url);
+  const fileName = await downloadImageAsPng(response.data[0].url);
+  const url = `${process.env.SERVER_URL}/gpt/image-generation/${fileName}`;
+
   return {
-    url,
-    localPath: '',
+    url: url,
+    openAIUrl: response.data[0].url,
+    revised_prompt: response.data[0].revised_prompt,
+  };
+};
+
+const editImage = async (
+  openai: OpenAI,
+  { prompt, originalImage, maskImage }: Options,
+) => {
+  // originalImage={{API_URL}}/image-generation/7974eda1-1b71-494c-a388-28ead64ebb89.png
+  // maskImage=Base64;AASDADASDASDASDASDASDASDASD
+  const pngImagePath = await downloadImageAsPng(originalImage);
+  const maskPath = await downloadBase64ImageAsPng(maskImage);
+
+  const response = await openai.images.edit({
+    model: 'dall-e-3',
+    prompt: prompt,
+    image: fs.createReadStream(pngImagePath),
+    mask: fs.createReadStream(maskPath),
+    n: 1,
+    size: '1024x1024',
+    response_format: 'url',
+  });
+
+  const fileName = await downloadImageAsPng(response.data[0].url);
+  const url = `${process.env.SERVER_URL}/gpt/image-generation/${fileName}`;
+
+  return {
+    url: url,
+    openAIUrl: response.data[0].url,
     revised_prompt: response.data[0].revised_prompt,
   };
 };
--- src/helpers/download-image-as-png.ts
+++ src/helpers/download-image-as-png.ts
@@ -1,5 +1,6 @@
 import * as path from 'path';
 import * as fs from 'fs';
+import * as sharp from 'sharp';
 import { v4 as uuidv4 } from 'uuid';
 import { InternalServerErrorException } from '@nestjs/common';
 
@@ -16,5 +17,29 @@ export const downloadImageAsPng = async (url: string) => {
   const imageNamePng = `${uuidv4()}.png`;
   const buffer = Buffer.from(await response.arrayBuffer());
 
-  fs.writeFileSync(`${folderPath}/${imageNamePng}`, buffer);
+  // fs.writeFileSync(`${folderPath}/${imageNamePng}`, buffer);
+
+  const completePath = path.join(folderPath, imageNamePng);
+
+  // Convert incoming image to PNG
+  await sharp(buffer).png().ensureAlpha().toFile(completePath);
+
+  return imageNamePng;
+};
+
+export const downloadBase64ImageAsPng = async (base64Image: string) => {
+  // Remover encabezado
+  base64Image = base64Image.split(';base64,').pop();
+  const imageBuffer = Buffer.from(base64Image, 'base64');
+
+  const folderPath = path.resolve('./', './generated/images/');
+  fs.mkdirSync(folderPath, { recursive: true });
+
+  const imageNamePng = `${uuidv4()}-64.png`;
+  const completePath = path.join(folderPath, imageNamePng);
+
+  // Transformar a RGBA, png // Así lo espera OpenAI
+  await sharp(imageBuffer).png().ensureAlpha().toFile(completePath);
+
+  return imageNamePng;
 };
